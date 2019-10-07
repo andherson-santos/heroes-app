42ed652e6ed35e1fd5e1dc51671129d69d6b1346
andhersonsantos <ansantos@bionexo.com>
2019-10-07 16:09:22 -0300

    test

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,37 @@
+import { HeroService } from './../hero.service';
+import { Hero } from './../hero';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  
+  heroes: Hero[];
+
+  selectedHero: Hero;
+  
+  constructor(
+    private heroService: HeroService
+  ) { }
+
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+    
+  }
+
+  getHeroes():void {
+    
+    this.heroService.getHeroes()    
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
